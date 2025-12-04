f7da54b058fb4165e82fe0737773401784db8900
Alan Shen <alan.shen27@gmail.com>
2025-12-04 20:36:50 +0100

    feat: new auton

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,10 @@
 ASSET(GOTOLONGGOAL_txt)
 ASSET(GETBALL_txt)
 
+ASSET(LOADBALLS_txt)
+ASSET(MOVETOLONGGOAL_txt)
+ASSET(PARKROBOTFROMLONGGOAL_txt)
+
 // Motor ports
 #define INTAKE_BASE_PRIMARY -7
 #define INTAKE_BASE_SECONDARY 18
@@ -69,14 +73,56 @@ void competition_initialize() {}
  * will be stopped. Re-enabling the robot will restart the task, not re-start it
  * from where it left off.
  */
-void autonomous() {
-    // set position to x:0, y:0, heading:0
-    chassis.setPose(0, 0, 0);
-    // turn to face heading 90 with a very long timeout
-    chassis.turnToHeading(90, 5000);
-    chassis.turnToHeading(0, 100000);
-}
 
+ void autonomous() {
+  lcd::print(1, "AUTON STARTED");
+  State state;
+
+	Motor intakeBasePrimary(INTAKE_BASE_PRIMARY, MOTOR_GEARSET);
+	Motor intakeBaseSecondary(INTAKE_BASE_SECONDARY, MOTOR_GEARSET);
+	Motor scorerLift(SCORER_LIFT_PORT, MOTOR_GEARSET);
+  
+	// Initialize pneumatics (ADI digital outputs)
+	adi::DigitalOut blockPiston(BLOCK_PISTON_PORT);
+	adi::DigitalOut middleGoalPiston(MIDDLE_GOAL_PISTON_PORT);
+	adi::DigitalOut loaderPiston(LOADER_PISTON_PORT);
+	adi::DigitalOut armPiston(ARM_PISTON_PORT);
+  
+	// Initialize handlers
+	IntakeHandler intake(intakeBasePrimary, intakeBaseSecondary, state.intake);
+	ScorerHandler scorer(scorerLift, state.scorer);
+	PneumaticsHandler pneumatics(blockPiston, middleGoalPiston, loaderPiston, armPiston,
+								 state.scorer, state.intake, state.loader, state.arm);
+  
+                //  58.69, -9.57, 97.067
+
+	chassis.setPose(58.69, -9.57, 97.067);  // Match GETBALL.txt starting point
+  pneumatics.init();
+	pneumatics.setBlock(true);
+	pneumatics.setMiddleGoal(false);
+	pneumatics.setLoader(true);
+  lcd::print(2, "Loading balls...");
+
+    // Follow path to get balls
+    chassis.follow(LOADBALLS_txt, 15, 5000, true);
+    lcd::print(2, "Loading balls done");
+	intake.toggle();
+	intake.update();
+	pros::delay(1000);
+	intake.toggle();
+	intake.update();
+	pros::delay(1000);
+
+	pneumatics.setBlock(false);
+	pneumatics.setLoader(false);    // Follow path to goal
+    chassis.follow(MOVETOLONGGOAL_txt, 15, 5000, true);
+	intake.toggle();
+  intake.update();
+  pros::delay(10000);
+  intake.toggle();
+  intake.update();
+  chassis.follow(PARKROBOTFROMLONGGOAL_txt, 15, 5000, true);
+}
 
 
 /**
@@ -148,12 +194,12 @@ void opcontrol() {
     // master.set_text(2, 0, "F" + std::to_string(round(forward * 100) / 100) +
     // " T" + std::to_string(round(turn * 100) / 100));
 
-    // Toggle drive direction
-    if (master.get_digital_new_press(DIGITAL_Y)) {
-      master.clear_line(1);
-      master.set_text(1, 0, "SCORER_REVERSE");
-      scorer.toggleDirection();
-    }
+    // // Toggle drive direction
+    // if (master.get_digital_new_press(DIGITAL_Y)) {
+    //   master.clear_line(1);
+    //   master.set_text(1, 0, "SCORER_REVERSE");
+    //   scorer.toggleDirection();
+    // }
 
     // Intake controls
     if (master.get_digital_new_press(DIGITAL_R1)) {
@@ -164,8 +210,9 @@ void opcontrol() {
 
     if (master.get_digital_new_press(DIGITAL_A)) {
       master.clear_line(1);
-      master.set_text(1, 0, "INTAKE_REVERSE");
+      master.set_text(1, 0, "SYSTEM_REVERSE");
       intake.toggleDirection();
+      scorer.toggleDirection();
     }
 
     // Scorer controls
